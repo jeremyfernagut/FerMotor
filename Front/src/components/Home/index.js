47c22f289d339946df14c7e51053156d3b278739
unknown <mrl.remi@gmail.com>
2020-11-06 16:45:55 +0100

    adding fake test data

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import data from '../../data/data_home';
 import './style-mobile.scss';
 import './style-desktop.scss';
 
-const Home = () => (
+const Home = ( props ) => {
+
+const { title, img, description } = props;
+return(
   <div className="main">
-    <div className="main_title">
-      <div className=""> La menuiserie </div>
-      <div className=""> L'équipe</div>
-      <div className></div>
+    <h1 className="main_title"> Bienvenue </h1>
+    <div className="main_corp">
+      <div className="corp_title"> {title} </div>
+      <div className="corp_img"> {img} </div>
+      <div className="corp_description"> {description} </div>
+
+    </div>
   </div>
-  </div>
-);
+)};
 
 export default Home;
---
+++ Front/src/data/data_home.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        title: 'La menuiserie';
+        img:'https://cdn.discordapp.com/attachments/691613572014276639/774293646874509322/workshop-4863393_640.jpg';
+        description:'Au service des particuliers et des professionnels, la Menuiserie Robin est reconnue pour son savoir-faire depuis 1987. Notre entreprise travaille régulièrement en partenariat avec des agences d’architecture et a par ailleurs rejoint en Janvier 2018 le groupement Treppenmeister, spécialiste de l’escalier design sur-mesure.';
+    };
+
+    {
+        title: 'Une équipe qualifiée';
+        img: 'https://cdn.discordapp.com/attachments/691613572014276639/774293412651597854/architect-3979490_640.jpg';
+        description:'Notre équipe (environ 40 personnes) est qualifiée, expérimentée et formée dans l’entreprise (stagiaire, apprenti, Compagnons du Devoir et du Tour de France, etc..).Nos collaborateurs mettent leur savoir-faire à votre service pour la réalisation de tous vos travaux.';
+    };
+
+    {
+        title: 'Nos méthodes';
+        img:'https://cdn.discordapp.com/attachments/691613572014276639/774293637404164106/meeting-2890414_640.jpg';
+        description:'La Menuiserie est reconnue pour la proximité et l’écoute du client, ses méthodes artisanales, le souci du travail bien fait et la sécurité permanente de ses collaborateurs.Un professionnel vous conseille selon vos envies, vos besoins et les différentes possibilités de réalisation de votre projet.';
+    };
+
+    {
+        title: ' études et devis';
+        img:'https://cdn.discordapp.com/attachments/691613572014276639/774293547829428234/insurance-539659_640.jpg';
+        description:'Nous travaillons à partir de plans ou directement en déplacement sur site, indispensable pour réaliser la prise de cote, visualiser l’environnement existant et établir une étude personnalisée adaptée à vos besoins et à votre budget.Tous nos travaux sont soumis à une garantie décennale et biennale.'
+    };
+]
