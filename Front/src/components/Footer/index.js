c4f34713855480a10f364719aaa5bc9102f03415
unknown <mrl.remi@gmail.com>
2020-11-06 17:10:18 +0100

    minor bug fix

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -9,8 +9,8 @@ const Footer = () => (
     <h1 className="footer_title"> Nos Coordonnées :</h1>
     <div className="footer_info">
 			<div className="footer_tel"><i className="fa fa-phone" /> 05.46.25.36.95 </div>
-    	<div className="footer_horraire"><i class="fa fa-clock-o" /> Lun-Ven 8:00 - 19.00</div>
-    	<div className="footer_adresse"><i class="fa fa-home" /> ZA Les Cerisiers Villedoux, 17230</div>
+    	<div className="footer_horraire"><i className="fa fa-clock-o" /> Lun-Ven 8:00 - 19.00</div>
+    	<div className="footer_adresse"><i className="fa fa-home" /> ZA Les Cerisiers Villedoux, 17230</div>
     </div>
   </div>
 )
--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import data from '../../data/data_home';
 import './style-mobile.scss';
 import './style-desktop.scss';
 
-const Home = ( props ) => {
-
-const { title, img, description } = props;
-return(
+const Home = () => (
   <div className="main">
-    <h1 className="main_title"> Bienvenue </h1>
+    <h1 className="main_title"> Bienvenu </h1>
     <div className="main_corp">
-      <div className="corp_title"> {title} </div>
-      <div className="corp_img"> {img} </div>
-      <div className="corp_description"> {description} </div>
-
+      <div className=""> La menuiserie </div>
+      <div className=""> L'équipe</div>
+      <div className=""> Nos méthodes</div>
+      <div className=""> Études et devis </div>
     </div>
   </div>
-)};
+);
 
 export default Home;
