82fd32cad330ac2c6951ebd438007dd998e45237
unknown <mrl.remi@gmail.com>
2020-11-14 10:32:28 +0100

    updating footer with new info

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -8,9 +8,10 @@ const Footer = () => (
   <div className="footer">
     <h1 className="footer_title"> Nos Coordonnées :</h1>
     <div className="footer_info">
-			<div className="footer_tel"><i className="fa fa-phone" /> 05-56-49-08-63 </div>
+			<a className="footer_tel" href="tel: +33556490863"><i className="fa fa-phone" /> 05.56.49.08.63 </a>
     	<div className="footer_horraire"><i className="fa fa-clock-o" /> Lundi-jeudi 07h30-12h30 / 14h-17h  - Vendredi 07h30-12h30 </div>
     	<div className="footer_adresse"><i className="fa fa-home" /> 5 rue Pierre Georges Latecoere 33850 LEOGNAN </div>
+      <a className="footer_mail" href="mailto:fermotor@wanadoo.fr"><i className="fa fa-envelope-o" /> fermotor@wanadoo.fr</a>
     </div>
   </div>
 )
