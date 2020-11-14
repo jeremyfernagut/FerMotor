558519e91b0e9e663e8115dfa0ae3bb13960531d
unknown <mrl.remi@gmail.com>
2020-11-14 14:04:26 +0100

    minor css bug fix

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -8,16 +8,18 @@ const Home = ( props ) => {
     <div className="main">
       <h1 className="main_welcome"> Bienvenue </h1>
       <div className="card">
-      {
-        list.map((cardObject) =>(
-        <div className="card_unit">
-          <h4> {cardObject.title} </h4>
-          <div className="">
-          <img  src={cardObject.img}/>  
-          </div>
-          <p> {cardObject.description} </p>
-        </div>
-        ))
+        {
+          list.map((cardObject) =>(
+            <div className="card_unit">
+              <h4> {cardObject.title} </h4>
+              <div className="">
+                <img  
+                 src={cardObject.img}
+                  className="card_img"/>  
+              </div>
+              <p> {cardObject.description} </p>
+            </div>
+          ))
       }
       </div>
     </div>
