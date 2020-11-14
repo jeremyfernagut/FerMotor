461c927985702cd74e3f0c4b14400f3f58611e33
unknown <mrl.remi@gmail.com>
2020-11-14 11:41:01 +0100

    working on the css & the legal mention

--- Front/src/components/Footer/index.js
+++ Front/src/components/Footer/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './style-mobile.scss';
 import './style-desktop.scss';
 
-// TODO : Inserer les vrais informations et véirifier l'utilité des className tel horraire adresse ( css empty ) 
+// // TODO : Inserer les vrais informations et véirifier l'utilité des className tel horraire adresse ( css empty ) 
 
 const Footer = () => (
   <div className="footer">
     <h1 className="footer_title"> Nos Coordonnées :</h1>
     <div className="footer_info">
-			<a className="footer_tel" href="tel: +33556490863"><i className="fa fa-phone" /> 05.56.49.08.63 </a>
-    	<div className="footer_horraire"><i className="fa fa-clock-o" /> Lundi-jeudi 07h30-12h30 / 14h-17h  - Vendredi 07h30-12h30 </div>
+		  <a className="footer_tel" href="tel: +33556490863"><i className="fa fa-phone" /> 05.56.49.08.63 </a>
+      <div className="footer_horraire">
+        <span><i className="fa fa-clock-o" /> Lundi-jeudi :</span>
+        <span>07h30-12h30 / 14h-17h</span>
+        <span>Vendredi : </span>
+        <span>07h30-12h30 </span>
+      </div>
     	<div className="footer_adresse"><i className="fa fa-home" /> 5 rue Pierre Georges Latecoere 33850 LEOGNAN </div>
       <a className="footer_mail" href="mailto:fermotor@wanadoo.fr"><i className="fa fa-envelope-o" /> fermotor@wanadoo.fr</a>
+    <div>Mentions légales</div>
     </div>
   </div>
 )
