9ff5679af205045f897fe8dd17f6d2e2a396bf6a
unknown <mrl.remi@gmail.com>
2020-11-18 11:13:17 +0100

    addition of a false bdd and dynamization of the whole

--- Front/src/components/Intervention/index.js
+++ Front/src/components/Intervention/index.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './style-mobile.scss';
 import './style-desktop.scss';
 
-const Intervention = () => (
-
-    <div>test services</div>
-);
+const Intervention = ( props ) => {
+  const { list } = props;
+  return (
+    <div>
+      <h1>Services : </h1>
+        <div>
+          {
+            list.map((cardObject) =>(
+              <div className="card_unit">
+              <h4 className="card_title"> {cardObject.title} </h4>
+              <div className="">
+                <img  
+                  src={cardObject.img}
+                  className="card_img"/>  
+              </div>
+              <p className="card_description"> {cardObject.supplier} </p>
+            </div>
+            ))
+          }
+        </div>
+    </div>
+  )
+}
 
 export default Intervention;
--- Front/src/data/service.js
+++ Front/src/data/service.js
@@ -1,5 +1,36 @@
 export default [
-    {
-        
-    }
+  {
+    id: '1',
+    title: 'Menuiserie Alu :',
+    img: 'https://cdn.discordapp.com/attachments/778173311783403560/778555708589342770/pexels-expect-best-323776.jpg',
+    supplier: 'Prod Alu 33 – Beychac et Caillau Prefal – Saint André de Cubzac FMA – Proches Nantes K-line – Proche Nantes',
+  },
+
+  {
+    id: '2',
+    title: 'Menuiserie Acier :',
+    img: 'https://cdn.discordapp.com/attachments/778173311783403560/778558215574913074/stairs-1229149_640.jpg',
+    supplier: 'F2M – Proche Nantes VD industry – Proche Strasbourg'
+  },
+
+  {
+    id: '3',
+    title: 'Menuiserie Pvc :',
+    img: 'https://cdn.discordapp.com/attachments/778173311783403560/778558872365039626/5444578593_e0a262a4ab_z.jpg',
+    supplier: 'Prefal – Saint André de Cubzac SAVS – Toulouse Ideal PVC – Proche Toulouse',
+  },
+
+  {
+    id: '4',
+    title: 'Volets roulants/porte de garage/portail :',
+    img: 'https://cdn.discordapp.com/attachments/778173311783403560/778556521231417344/porte_garage_1.jpg',
+    supplier: 'GAB33 – Mérignac France Fermetures – Proche Montluçon'
+  },
+
+  {
+    id: '5',
+    title: ' Stores/Brise-soleil/Rideaux : ',
+    img: 'https://cdn.discordapp.com/attachments/778173311783403560/778560277301035008/volets-stores.jpg',
+    supplier: 'La Venitienne –  Proche Angoulême ABW/Enjoyourspace – Proche Nantes France Fermetures – Proche Montluçon',
+  }
 ]
