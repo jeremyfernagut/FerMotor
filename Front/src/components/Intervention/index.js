02198af6266e69dde177b0a21fe895b8e56c0c74
unknown <mrl.remi@gmail.com>
2020-11-20 11:16:56 +0100

    added some text & css

--- Front/src/components/Intervention/index.js
+++ Front/src/components/Intervention/index.js
@@ -10,6 +10,14 @@ const Intervention = ( props ) => {
   return (
     <div className="intervention">
       <h1 className="intervention_title">Services : </h1>
+      <p className="intervention_text">Faite confiance à votre installateur pour vous guider dans la sélection ! Nous vous aidons à choisir vos produits, pour vous assurer une pose de menuiseries de qualité. Faisons en sorte que votre projet d’installation soit une réussite !</p>
+      <p className="intervention_text">Les matériaux disponibles pour la fabrication ont chacun leurs avantages. Leur sélection va dépendre de vos besoins et de votre budget :</p>
+      <ul className="intervention_ul">
+        <li> le PVC est facile d’entretien et d’un bon rapport qualité/prix,</li>
+        <li> l’aluminium est robuste, léger et disponible en divers coloris,</li>
+        <li>le bois est chaleureux, esthétique et naturellement isolant,</li>
+        <li>et le mixte alu-bois allie les qualité de ces deux matériaux.</li>
+      </ul>
       <div className="intervention_card">
         {
           list.map((cardObject) =>(
